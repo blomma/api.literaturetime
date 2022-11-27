d1aa36b1e66338367dcfa7104e32db91b86d06eb
Mikael Hultgren <blomma@artsoftheinsane.com>
2022-11-27 21:59:30 +0100

    Include traceparent logging

--- src/API.LiteratureTime.API/Program.cs
+++ src/API.LiteratureTime.API/Program.cs
@@ -38,6 +38,7 @@ builder.Services.AddHttpLogging(logging =>
     logging.RequestHeaders.Add("X-Forwarded-Server");
     logging.RequestHeaders.Add("X-Real-Ip");
     logging.RequestHeaders.Add("Upgrade-Insecure-Requests");
+    logging.RequestHeaders.Add("traceparent");
     logging.LoggingFields = HttpLoggingFields.All;
     logging.RequestBodyLogLimit = 4096;
     logging.ResponseBodyLogLimit = 4096;
