5270fd62752a885c20cd03669b24de75e78b23ea
Mikael Hultgren <blomma@artsoftheinsane.com>
2023-05-02 19:02:30 +0200

    Remove version prefix from route

--- src/API.LiteratureTime.API/RouterModules/LiteratureRouterModule.cs
+++ src/API.LiteratureTime.API/RouterModules/LiteratureRouterModule.cs
@@ -10,7 +10,6 @@ public class LiteratureRouterModule : IRouterModule
     public void AddRoutes(IEndpointRouteBuilder app)
     {
         var group = app.MapGroup("/api/literature").AddEndpointFilter<ApiExceptionFilter>();
-        var group2 = app.MapGroup("/api/2.0/literature").AddEndpointFilter<ApiExceptionFilter>();
 
         group
             .MapGet(
@@ -36,30 +35,5 @@ public class LiteratureRouterModule : IRouterModule
                 }
             )
             .WithName("GetLiteratureTime");
-
-        group2
-            .MapGet(
-                "/{hour}/{minute}",
-                ([FromServices] ILiteratureService literatureService, string hour, string minute) =>
-                {
-                    return literatureService.GetRandomLiteratureTimeAsync(hour, minute);
-                }
-            )
-            .WithName("V2GetRandomLiteratureTime");
-
-        group2
-            .MapGet(
-                "/{hour}/{minute}/{hash}",
-                (
-                    [FromServices] ILiteratureService literatureService,
-                    string hour,
-                    string minute,
-                    string hash
-                ) =>
-                {
-                    return literatureService.GetLiteratureTimeAsync(hash);
-                }
-            )
-            .WithName("V2GetSpecificLiteratureTime");
     }
 }
